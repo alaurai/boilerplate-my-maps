0ba7182c90cab53e33863250c739d0d53d7fb7f1
A.Laura Ivani <alauraivani@gmail.com>
2022-09-10 21:07:45 -0300

    feat: custom boilerplate

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,22 +1,3 @@
-import * as S from './styles'
-
 export default function Main() {
-  return (
-    <>
-      <S.Wrapper>
-        <S.Logo
-          src="/img/logo.svg"
-          alt="Imagem de um átomo e React Avançado escrito ao lado."
-        />
-        <S.Title>React Avançado</S.Title>
-        <S.Description>
-          TypeScript, ReactJS, NextJS e Styled Components
-        </S.Description>
-        <S.Illustration
-          src="/img/hero-illustration.svg"
-          alt="Um desenvolvedor de frente para uma tela com código."
-        />
-      </S.Wrapper>
-    </>
-  )
+  return <h1>Hello NextJS!</h1>
 }
